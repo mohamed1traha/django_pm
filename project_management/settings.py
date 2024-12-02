f2ee2571a8bfe62b205a109f0a40e0603200a935
mohamed <ttmm187110@gmail.com>
2024-12-02 13:40:01 +0300

    Add requirements.txt file

--- project_management/settings.py
+++ project_management/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,7 +86,7 @@ DATABASES = {
         'USER' : 'postgres',
         'PASSWORD': 'MOHAMED',
         'HOST' : 'localhost',
-        'PORT' : ''
+        'PORT' : os.getenv('PORT', 8000)
 
     }
 }
