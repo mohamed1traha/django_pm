bc4e5f575bbc5634312381ac53b874e638da6c24
mohamed <ttmm187110@gmail.com>
2025-01-27 14:37:43 +0300

    search and pagination

--- project/views.py
+++ project/views.py
@@ -10,7 +10,15 @@ from django.urls import reverse_lazy ,reverse
 class Project_View(ListView):
     model = models.Project
     template_name = 'project/list.html'
-    paginate_by = 6
+    paginate_by = 3
+        
+    def get_queryset(self) :
+        query_set= super().get_queryset()
+        where= {}
+        q= self.request.GET.get('q',None)
+        if q : where['title__icontains'] =q
+        return query_set.filter(**where)
+
 
 class ProjectCreateView(CreateView):
     model = models.Project
